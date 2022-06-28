481787b5dd7e122221cc308d712e51282dcbcbc9
Arturo <amontejo@ujaen.es>
2022-06-29 00:07:29 +0200

    ready for deployment

---
+++ precache-manifest.9edf9aa336c0bfff2c01bb78a1dc60d0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b99b94187b2c1c49670",
+    "url": "/static/css/main.6c02af24.chunk.css"
+  },
+  {
+    "revision": "5b99b94187b2c1c49670",
+    "url": "/static/js/main.5b99b941.chunk.js"
+  },
+  {
+    "revision": "3923d1587caf1456eef0",
+    "url": "/static/css/1.58ea3d33.chunk.css"
+  },
+  {
+    "revision": "3923d1587caf1456eef0",
+    "url": "/static/js/1.3923d158.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7b1ff8113bbf5bd082a2cbf2cbbb541e",
+    "url": "/static/media/stairs.7b1ff811.jpg"
+  },
+  {
+    "revision": "b57a58905370284f1499d1a35f5d2c96",
+    "url": "/static/media/bootstrap-icons.b57a5890.woff2"
+  },
+  {
+    "revision": "a6a4862c8f0a585c6c1f6d5aa07b761c",
+    "url": "/static/media/bootstrap-icons.a6a4862c.woff"
+  },
+  {
+    "revision": "fd719f8aa665ce7fa1b9ab48465eba82",
+    "url": "/index.html"
+  }
+];
---
+++ precache-manifest.efd2b5b7bc9eb77165c8932efef6a528.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c6b46cc882822c9b8b5",
+    "url": "/static/css/main.6c02af24.chunk.css"
+  },
+  {
+    "revision": "5c6b46cc882822c9b8b5",
+    "url": "/static/js/main.5c6b46cc.chunk.js"
+  },
+  {
+    "revision": "3923d1587caf1456eef0",
+    "url": "/static/css/1.58ea3d33.chunk.css"
+  },
+  {
+    "revision": "3923d1587caf1456eef0",
+    "url": "/static/js/1.3923d158.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7b1ff8113bbf5bd082a2cbf2cbbb541e",
+    "url": "/static/media/stairs.7b1ff811.jpg"
+  },
+  {
+    "revision": "b57a58905370284f1499d1a35f5d2c96",
+    "url": "/static/media/bootstrap-icons.b57a5890.woff2"
+  },
+  {
+    "revision": "a6a4862c8f0a585c6c1f6d5aa07b761c",
+    "url": "/static/media/bootstrap-icons.a6a4862c.woff"
+  },
+  {
+    "revision": "64f723ce67d6a63dd095ef4b342c6772",
+    "url": "/index.html"
+  }
+];
