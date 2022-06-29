bdf0e7c78c5983f2368f9862e91a77a788d22c48
Arturo <amontejo@ujaen.es>
2022-06-29 16:51:26 +0200

    new version for the web

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e77c16f0bec7e83b1ff7c15a1800965.js"
+  "/precache-manifest.a7b892208032fc28ac08cf50372a38f7.js"
 );
 
 workbox.clientsClaim();
