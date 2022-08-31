a9e01191ccc17ea603541b88e7e0d44bbb53a698
Arturo Montejo Ráez <amontejo@ujaen.es>
2022-08-31 13:58:02 +0200

    updated

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.368b74bc604aad9f653661fb72719894.js"
+  "/precache-manifest.687722bae22971f9898f35e3af6586b9.js"
 );
 
 workbox.clientsClaim();
