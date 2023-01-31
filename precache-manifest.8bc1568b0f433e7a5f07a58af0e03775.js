ac16167d0c71b1168f9c91385a77e59eafd77896
Arturo Montejo Ráez <amontejo@ujaen.es>
2023-01-31 14:07:05 +0100

    updated

---
+++ precache-manifest.8bc1568b0f433e7a5f07a58af0e03775.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b1ff8113bbf5bd082a2cbf2cbbb541e",
+    "url": "/levelup_app/static/media/stairs.7b1ff811.jpg"
+  },
+  {
+    "revision": "b57a58905370284f1499d1a35f5d2c96",
+    "url": "/levelup_app/static/media/bootstrap-icons.b57a5890.woff2"
+  },
+  {
+    "revision": "a6a4862c8f0a585c6c1f6d5aa07b761c",
+    "url": "/levelup_app/static/media/bootstrap-icons.a6a4862c.woff"
+  },
+  {
+    "revision": "e81424c839a438a49010",
+    "url": "/levelup_app/static/js/runtime~main.e81424c8.js"
+  },
+  {
+    "revision": "1ed97a3101b7045ae445",
+    "url": "/levelup_app/static/js/main.1ed97a31.chunk.js"
+  },
+  {
+    "revision": "c9da0c068d590c574b17",
+    "url": "/levelup_app/static/js/2.c9da0c06.chunk.js"
+  },
+  {
+    "revision": "9b6a3eeb691835529c4c",
+    "url": "/levelup_app/static/js/1.9b6a3eeb.chunk.js"
+  },
+  {
+    "revision": "1ed97a3101b7045ae445",
+    "url": "/levelup_app/static/css/main.60fa23ef.chunk.css"
+  },
+  {
+    "revision": "c9da0c068d590c574b17",
+    "url": "/levelup_app/static/css/2.420f77ce.chunk.css"
+  },
+  {
+    "revision": "f98b09aad867093e7a3194164429a37a",
+    "url": "/levelup_app/index.html"
+  }
+];
